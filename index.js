82318f4add9c07b537766fa50b0d5d4750e4f8b5
XhmikosR <xhmikosr@gmail.com>
2026-06-11 15:17:41 +0300

    index.js: switch to async/await

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 import {execa, execaSync} from 'execa';
 import {isexe, sync as isexeSync} from 'isexe';
 
-const binCheck = (bin, args) => {
+const binCheck = async (bin, args) => {
 	if (!Array.isArray(args)) {
 		args = ['--help'];
 	}
 
-	return isexe(bin)
-		.then(works => {
-			if (!works) {
-				throw new Error(`Couldn't execute the "${bin}" binary. Make sure it has the right permissions.`);
-			}
+	const works = await isexe(bin);
+	if (!works) {
+		throw new Error(`Couldn't execute the "${bin}" binary. Make sure it has the right permissions.`);
+	}
 
-			return execa(bin, args);
-		})
-		.then(result => result.exitCode === 0);
+	const result = await execa(bin, args);
+
+	return result.exitCode === 0;
 };
 
 binCheck.sync = (bin, args) => {
